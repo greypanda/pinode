337e492bb2247db49148a4909f846412e57cec8b
John <john@pandavista.com>
2020-10-04 01:29:16 +0100

    first py file

---
+++ node.py
@@ -0,0 +1,15 @@
+"""
+    node.py is a remote node that performs home automation
+    tasks using MQTT. 
+
+    Major tasks are:
+
+        home assistant discovery
+        DHT22 temperature and humidity sensor
+        DS18b20 temperature sensors
+        Relays to control 120VAC devices
+        Automatic updates
+        
+
+
+"""
